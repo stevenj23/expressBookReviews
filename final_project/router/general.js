4654931f2d7d85c925b7a38e90514cb7c3671efd
stevenj23 <145675997+stevenj23@users.noreply.github.com>
2026-03-18 14:35:59 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,6 @@ const public_users = express.Router();
 
 public_users.post("/register", (req,res) => {
   //Write your code here
-//   return res.status(300).json({message: "Yet to be implemented"});
 
     const username = req.body.username;
     const password = req.body.password;
@@ -36,14 +35,13 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',async function (req, res) {
   //Write your code here
-//   return res.status(300).json({message: "Yet to be implemented"});
     return res.send(JSON.stringify({books}, null, 4));
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
-//    return res.status(300).json({message: "Yet to be implemented"});
+
    const isbn = req.params.isbn;
 
    const filteredBooks = Object.values(books).filter(
@@ -57,7 +55,6 @@ public_users.get('/isbn/:isbn',async function (req, res) {
 // Get book details based on author
 public_users.get('/author/:author', async function (req, res) {
   //Write your code here
-//   return res.status(300).json({message: "Yet to be implemented"});
     const author = req.params.author;
 
     const filteredBooks = Object.values(books).filter(
@@ -70,7 +67,7 @@ public_users.get('/author/:author', async function (req, res) {
 // Get all books based on title
 public_users.get('/title/:title', async function (req, res) {
   //Write your code here
-    // return res.status(300).json({message: "Yet to be implemented"});
+    
     const title = req.params.title;
 
     const filteredBooks = Object.values(books).filter(
@@ -84,7 +81,6 @@ public_users.get('/title/:title', async function (req, res) {
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-//   return res.status(300).json({message: "Yet to be implemented"});
 
     const isbn = req.params.isbn;
 
