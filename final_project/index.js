817d45821edc73fc98a999f7bb07be912c9a8cfd
stevenj23 <145675997+stevenj23@users.noreply.github.com>
2026-03-18 23:08:35 -0400

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -20,6 +20,44 @@ app.use("/customer/auth/*", function (req, res, next) {
 
 });
 
+
+// app.use("/customer/auth/*", function auth(req,res,next){
+// //Write the authenication mechanism here
+
+//     // if (req.session.authorization) { // Get the authorization object stored in the session
+//     //     token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
+//     //     jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
+//     //     if (!err) {
+//     //         req.user = user;
+//     //         next();
+//     //     } else {
+//     //         return res.status(403).json({ message: "User not authenticated" });
+//     //     }
+//     //     });
+//     // } else {
+//     //     return res.status(403).json({ message: "User not logged in" });
+//     // }
+//     const token =
+//         req.headers["authorization"]?.split(" ")[1] || // Bearer token
+//         req.body.token;
+
+//     if (!token) {
+//         return res.status(403).json({ message: "Token missing" });
+//     }
+
+//     jwt.verify(token, "access", (err, user) => {
+//         if (err) {
+//             console.log("JWT ERROR:", err.message); // 👈 add this
+//             return res.status(403).json({ message: "User not authenticated" });
+//         }
+    
+//         console.log("DECODED USER:", user); // 👈 confirm success
+//         req.user = user;
+//         next();
+//     });   
+
+// });
+ 
 const PORT =5000;
 
 app.use("/customer", customer_routes);
