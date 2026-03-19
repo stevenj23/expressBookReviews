a70dde2f38aae6ade34a8ada844f833f0114518f
stevenj23 <145675997+stevenj23@users.noreply.github.com>
2026-03-18 21:07:57 -0400

    Update index.js
    Final

--- final_project/index.js
+++ final_project/index.js
@@ -10,25 +10,16 @@ app.use(express.json());
 
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function (req, res, next) {
 
-    if (req.session.authorization) { // Get the authorization object stored in the session
-        token = req.session.authorization['accessToken']; // Retrieve the token from authorization object
-        jwt.verify(token, "access", (err, user) => { // Use JWT to verify token
-        if (!err) {
-            req.user = user;
-            next();
-        } else {
-            return res.status(403).json({ message: "User not authenticated" });
-        }
-        });
+    if (req.session && req.session.authorization) {
+        return next();
     } else {
         return res.status(403).json({ message: "User not logged in" });
     }
 
 });
- 
+
 const PORT =5000;
 
 app.use("/customer", customer_routes);
